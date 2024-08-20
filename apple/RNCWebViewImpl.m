ce361b614a2434c9bed50b63c3f77a22ab756809
dylan-bronson-sh <dylan.bronson@stubhub.com>
2024-08-20 10:34:48 -0400

    feat (iOS):Allow light mode refresh control ios (#3479)
    * Allow light mode refresh control ios
    
    * change back docs
    
    (cherry picked from commit 2a1b9ad4b2fb19bd289f3c3a7e87925a7c93107c)

--- apple/RNCWebViewImpl.m
+++ apple/RNCWebViewImpl.m
@@ -1604,6 +1604,9 @@ didFinishNavigation:(WKNavigation *)navigation
 {
   UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
   _refreshControl = refreshControl;
+  if(_refreshControlLightMode) {
+    [refreshControl setTintColor:[UIColor whiteColor]];
+  }
   [_webView.scrollView addSubview: refreshControl];
   [refreshControl addTarget:self action:@selector(pullToRefresh:) forControlEvents: UIControlEventValueChanged];
 }
